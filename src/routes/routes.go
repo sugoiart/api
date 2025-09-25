7a7ff14ffad11c2eebc1128d19e5cc65868ee23a
jckli <me@jackli.dev>
2025-09-25 01:20:23 +0000

    fix: add / lol

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -13,5 +13,5 @@ func InitRoutes(router *httprouter.Router) {
 	router.GET("/v1/art/random", art.Random)
 	router.GET("/v1/art/all", art.All)
 	router.GET("/v1/art/sha/:sha", art.BySHA)
-	router.GET("v1/art/stats", art.Stats)
+	router.GET("/v1/art/stats", art.Stats)
 }
